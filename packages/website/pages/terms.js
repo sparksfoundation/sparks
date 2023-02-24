73ecd650da9be5b0521f5b360c3fb407fbe7f346
sparksdev <dev@sparks.dev>
2023-02-24 13:49:13 -0500

    add terms

---
+++ packages/website/pages/terms.js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SPARKS Foundation | Terms</title>
+        <meta name="description" content="SPARKS Foundation | Terms" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap" rel="stylesheet" />
+      </Head>
+      <main className="terms">
+        <svg width="32" height="32" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M63.8848 30.0283L35.6849 1.82843C34.1228 0.26633 31.5901 0.266327 30.028 1.82842L1.82811 30.0283C0.266018 31.5904 0.266014 34.1231 1.82811 35.6852L30.028 63.8851C31.5901 65.4472 34.1228 65.4472 35.6849 63.8851L63.8848 35.6852C65.4469 34.1231 65.4469 31.5904 63.8848 30.0283Z" fill="#F9F9F9" />
+        </svg>
+        <h1>
+          SPARKS Foundation - Terms of Use
+        </h1>
+        <h3>
+          Last updated: February 24th, 2023
+        </h3>
+        <p>
+{`Please read these Terms of Use ("Terms") carefully as they govern your use of the websites and services provided by Sparks Foundation and Sparks DAO ("Services"). These Terms represent a binding agreement between you and Sparks Foundation and Sparks DAO. By accessing, using, or participating in the Services, you acknowledge that you have read, understood, and agree to be bound by these Terms.
+
+Acceptance of Terms. You accept these Terms when you access or use the Services or any portion of the Services. If you do not agree to these Terms, do not use the Services.
+
+Compliance with Regulations. By using the Services, you covenant, represent, and warrant that you comply with all related regulatory conditions, including but not limited to sanctions compliance and anti-money laundering (AML) procedures, and adhere to any such related covenants as provided from time to time by Sparks Foundation and Sparks DAO, which shall in any case be no less stringent than the ones in place on the date of the Foundation’s formation and of the ratification of these Bylaws.
+
+Intellectual Property. The Services contain proprietary information and content that is protected by intellectual property laws and treaties. All trademarks, service marks, and trade names are proprietary to Sparks Foundation and Sparks DAO.
+
+Limitations of Liability. Sparks Foundation and Sparks DAO shall not be liable for any damages, including but not limited to direct, indirect, incidental, consequential, or punitive damages arising out of or in connection with the Services. This limitation of liability applies to all causes of action, whether based on contract, tort, or any other legal theories.
+
+Termination. Sparks Foundation and Sparks DAO may, in their sole discretion, terminate or suspend your access to all or part of the Services, with or without notice, for any reason, including breach of these Terms.
+
+Governing Law. These Terms shall be governed by and construed in accordance with the laws of Cayman Islands. Any dispute arising out of or in connection with these Terms shall be subject to the exclusive jurisdiction of the courts of Cayman Islands.
+
+Changes to the Terms. Sparks Foundation and Sparks DAO reserve the right, in their sole discretion, to modify or update these Terms at any time. If Sparks Foundation and Sparks DAO make material changes to these Terms, they will notify you by email or by posting a notice on the Services.
+
+Entire Agreement. These Terms constitute the entire agreement between you and Sparks Foundation and Sparks DAO regarding the Services and supersede all prior agreements and understandings.
+
+Contact Information. If you have any questions or concerns about these Terms, please contact Sparks Foundation and Sparks DAO at operations@sparks.foundation.`}
+</p>
+
+      </main>
+    </>
+  )
+}
